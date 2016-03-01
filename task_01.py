47a8b2f8fbabf1a576009a51a079364d8db058d5
pcwebb <pamela.webb@spsmail.cuny.edu>
2016-03-01 01:36:20 +0000

    Uploading 1

--- task_01.py
+++ task_01.py
@@ -2,19 +2,7 @@
 # -*- coding: utf-8 -*-
 """Defining a list and tuple using standard constructors."""
 
-ELEMENTS = (None,
-            'Hydrogen',
-            'Helium',
-            'Lithium',
-            'Beryllium',
-            'Boron',
-            'Carbon'
-           )
-
-OPERATIONS_ORDER = ['Parentheses',
-                    'Exponents',
-                    'Multiplication',
-                    'Division',
-                    'Addition',
-                    'Subtraction'
-                   ]
+ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium',
+            'Beryllium', 'Boron', 'Carbon')
+OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication',
+                    'Division', 'Addition', 'Subtraction']
