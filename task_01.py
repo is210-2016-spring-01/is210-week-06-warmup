fd18b1f5495b79d2c759e9e022308359c18d9f8b
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-03-01 23:01:46 +0000

    Added new file

---
+++ task_01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A small docstring for a module"""
+
+ELEMENTS = (None, 'Hyrdrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron',
+            'Carbon')
+OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division',
+                    'Addition', 'Subtraction']
