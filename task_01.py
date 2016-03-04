412a45d56118e629184ae783dc5d52e536f4ceb5
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-03-04 09:02:16 +0000

    working on it'

---
+++ task_01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 *-*
+"""Task 01"""
+
+
+ELEMENTS = (None, 'Hydrogen', 'Helium',
+            'Lithium', 'Beryllium', 'Boron', 'Carbon')
+
+OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication',
+                    'Division', 'Addition', 'Subtraction']
---
+++ task_05.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 05"""
+
+
+def flip_keys(*to_flip):
+    """1.  A list named ``to_flip``. This list is assumed to have nested,
+        immutable sequences inside it, eg: ``[(1, 2, 3), 'hello']``"""
+    #counter = 0
+    for reverse in to_flip:
+        reversed(to_flip)
+        #to_flip.reverse()
+        #tuple_reverse = to_flip[::-1] 
+        #list_reverse.append(tuple_reverse)
+        #to_flip = to_flip[::-1]
+    #return counter
+    #return tuple_reverse
+        return to_flip
