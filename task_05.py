7d7eec61fd8fa950872669c4f8aa0f7020292767
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-03-04 11:48:43 +0000

    working on it'

--- task_05.py
+++ task_05.py
@@ -6,13 +6,12 @@
 def flip_keys(*to_flip):
     """1.  A list named ``to_flip``. This list is assumed to have nested,
         immutable sequences inside it, eg: ``[(1, 2, 3), 'hello']``"""
-    #counter = 0
-    for reverse in to_flip:
-        reversed(to_flip)
-        #to_flip.reverse()
-        #tuple_reverse = to_flip[::-1] 
-        #list_reverse.append(tuple_reverse)
-        #to_flip = to_flip[::-1]
-    #return counter
-    #return tuple_reverse
-        return to_flip
+    counter = 0
+    for flip in to_flip:
+        flip1 = flip[1][::-1]
+        #flip = list(flip[1]).reverse()
+        flip2 = flip[0][::-1]
+        #flip = flip[0][::-1].extend(flip[1:3])
+        #flip = flip1.reverse()
+        counter +=1
+        return flip2, flip1
