dd27c23caf00117079737c45f122159b7ba4570d
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-03-04 12:04:08 +0000

    working on it'

--- task_05.py
+++ task_05.py
@@ -10,10 +10,8 @@ def flip_keys(*to_flip):
     for flip in to_flip:
         flip1 = flip[1][::-1]
         flip2 = flip[0][::-1]
-        counter +=1
+        counter += 1
         flip = list()
         flip.append(flip2)
         flip.append(flip1)
-        #return flip, flip2, flip1, counter == len(to_flip)
-        return flip
-    
+        return flip, flip2, flip1, counter == len(to_flip)
