d4ad6b6e23581473144d1799d33c8bd724f769ae
ct3080a <coleen.thomas@spsmail.cuny.edu>
2016-03-05 07:43:07 +0000

    partial attempt at for loop

---
+++ task_04.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The for loop"""
+
+def process_data(data):
+    """Tuple of numbers or list.
+
+    Args:
+        data: list or tuple of numbers
+
+    Returns:
+        i. total sum of the data
+        ii. average of data
+
+    Examples:
+        >>> process_data([1, 2, 3])
+        (6, 2.0)
+    """
+
+    return
