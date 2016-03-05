badae287db9c29eb9e12245f824ed98df61b267f
ct3080a <coleen.thomas@spsmail.cuny.edu>
2016-03-05 02:09:11 +0000

    list and tuple intro

---
+++ task_01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Intro to tuples and lists"""
+
+ELEMENTS = (None,
+            'Hydrogen',
+            'Helium',
+            'Lithium',
+            'Beryllium',
+            'Boron',
+            'Carbon')
+
+OPERATIONS_ORDER = ['Parentheses',
+                    'Exponents',
+                    'Multiplication',
+                    'Division',
+                    'Addition',
+                    'Subtraction']
